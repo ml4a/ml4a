fc7fbdfd4863149096a0dcb3e1481a6b946e0dc0
Gene Kogan <kogan.gene@gmail.com>
2016-10-20 02:01:10 +0200

    changes to run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,7 @@
 CONTAINER="ml4a-guides"
 IMAGE="genekogan/$CONTAINER"
 IMAGE_FILE="$CONTAINER.tar"
-VM="default"
 JUPYTER_PORT="8888"
-
-#source start-docker.sh
 
 if ! ( docker images | grep "$IMAGE" &>/dev/null ) ; then
 	if [ -e $IMAGE_FILE ]; then
@@ -19,30 +16,6 @@ if ! ( docker images | grep "$IMAGE" &>/dev/null ) ; then
 	fi
 fi
 
-HOST_IP=0.0.0.0 #docker-machine ip $VM
-
-# this might be better as an ssh followed by a deletion on exit
-if ( docker stats --no-stream=true $CONTAINER &>/dev/null ) ; then
-	echo "The container is already running, stopping it..."
-	docker stop $CONTAINER &>/dev/null
-	if ( docker stats --no-stream=true $CONTAINER &>/dev/null ) ; then
-		echo "The container still exists, removing it..."
-		docker rm $CONTAINER &>/dev/null
-	fi
-fi
-
-openavailable() {
-	# echo "Opening http://$1:$2/ once it is available..."
-	# wait for nc to connect to the port
-	# todo: windows?
-	until nc -vz "$1" "$2" &>/dev/null; do
-		sleep 1
-	done
-	# todo: windows and linux?
-	open "http://$1:$2/"
-}
-
-#openavailable $HOST_IP $JUPYTER_PORT &
-
+HOST_IP=0.0.0.0
 DIR=`pwd`
 docker run --publish $JUPYTER_PORT:$JUPYTER_PORT --volume=$DIR/:$DIR/ -ti genekogan/ml4a-guides /bin/bash -c "ln /dev/null /dev/raw1394 ; jupyter notebook --no-browser --ip=$HOST_IP --port $JUPYTER_PORT"
