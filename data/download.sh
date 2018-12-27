0aab8ab728849227b197cbe7830c07a50fc46ac7
Kyle Mathewson <kylemath@gmail.com>
2018-12-27 12:09:55 -0700

    changed wget to curl

--- data/download.sh
+++ data/download.sh
@@ -1,24 +1,24 @@
 
 # for most image processing notebooks, get 101_Object_Categories
 echo "Downloading 101_Object_Categories for image notebooks"
-wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
+curl -L -o 101_ObjectCategories.tar.gz --progress-bar http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
 tar -xzf 101_ObjectCategories.tar.gz
 rm 101_ObjectCategories.tar.gz
 
 # for eigenfaces, get labeled faces in the wild
 echo "Downloading LFW dataset for faces in the wild"
-wget http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz
+curl -L -o lfw-funneled.tgz --progress-bar http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz
 tar -xzf lfw-funneled.tgz 
 rm lfw-funneled.tgz 
 
 # drum samples
 echo "Downloading drum samples for audio t-SNE notebook"
-wget http://ivcloud.de/index.php/s/QyDXk1EDYDTVYkF/download -O drums.rar
+curl -L -o drums.rar --progress-bar http://ivcloud.de/index.php/s/QyDXk1EDYDTVYkF/download
 unrar x -o+ drums.rar
 rm drums.rar
 
 # drum samples
 echo "Downloading Reuters dataset for text retrieval notebook"
-wget http://disi.unitn.it/moschitti/corpora/Reuters21578-Apte-90Cat.tar.gz
+curl -L -o Reuters21578-Apte-90Cat.tar.gz --progress-bar wget http://disi.unitn.it/moschitti/corpora/Reuters21578-Apte-90Cat.tar.gz
 tar -xzf Reuters21578-Apte-90Cat.tar.gz
 rm Reuters21578-Apte-90Cat.tar.gz
