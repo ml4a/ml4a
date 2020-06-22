08900a51db6e646204d69b811d7f279b61250e89
Gene Kogan <kogan.gene@gmail.com>
2020-06-22 13:57:52 -0400

    turn into python package (untested)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='neuralarttools',
+      version='0.1',
+      description='A collection of various utilities mainly useful for pixel-optimization based neural arts, including masks and image-distortion functions, video processing, datasets, and convenience functions for Jupyter notebooks.',
+      url='http://github.com/genekogan/neural-art-tools',
+      author='Gene Kogan',
+      author_email='gene@genekogan.com',
+      license='LGPL 2.0',
+      packages=['neuralarttools'],
+      zip_safe=False)
+
