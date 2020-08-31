96f0fd10d257b156d941f2c95deb1a0d73e0c610
Gene Kogan <kogan.gene@gmail.com>
2020-08-31 03:10:46 -0400

    submodules

---
+++ ml4a/models/__init__.py
@@ -0,0 +1,4 @@
+print("import ml4a/models/__init__")
+
+from . import stylegan2
+
---
+++ ml4a/models/submodules/__init__.py
@@ -0,0 +1,8 @@
+print("import ml4a/models/submodules/__init__")
+#from . import stylegan2
+
+#from .stylegan2 import *
+
+
+
+from . import stylegan2
