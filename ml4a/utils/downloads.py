baa9dce5ad070d69b75468ab25c5d9cf5bf919bd
Gene Kogan <kogan.gene@gmail.com>
2020-10-21 18:40:01 -0400

    add photosketch

--- ml4a/utils/downloads.py
+++ ml4a/utils/downloads.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from collections import OrderedDict
 from torch.utils import model_zoo
+import zipfile
 import torch
 import os
 import requests
@@ -17,38 +18,43 @@ def get_ml4a_downloads_folder():
     Path(ml4a_downloads).mkdir(parents=True, exist_ok=True)
     return ml4a_downloads
 
-    
-def download_from_gdrive(gdrive_fileid, output_path, overwrite=False):
-    ml4a_downloads = get_ml4a_downloads_folder()
-    folder, filename = os.path.split(output_path)
-    output_folder = os.path.join(ml4a_downloads, folder)
-    output_filename = os.path.join(output_folder, filename)
-    if not os.path.exists(output_filename) or overwrite:
+
+def unzip(zip_file, output_folder, erase_zipfile=True):
+    with zipfile.ZipFile(zip_file, 'r') as zipref:
+        zipref.extractall(output_folder)
+        zipref.close()
+    if erase_zipfile:
+        os.remove(zip_file)
+
+
+def download_from_gdrive(gdrive_fileid, output_path, zip_file=False, overwrite=False):
+    output_folder, output_filename, output_exists = __process_output_path__(output_path, zip_file)
+    if not output_exists or overwrite:
         Path(output_folder).mkdir(parents=True, exist_ok=True)
         gdrive_url = 'https://drive.google.com/uc?id=%s'%gdrive_fileid
         gdown.download(gdrive_url, output_filename, quiet=False)
-    return output_filename
+        if zip_file:
+            unzip(output_filename, output_folder)
+    output = output_folder if zip_file else output_filename
+    return output
 
 
-def download_data_file(url, output_path, overwrite=False):
-    ml4a_downloads = get_ml4a_downloads_folder()
-    folder, filename = os.path.split(output_path)
-    output_folder = os.path.join(ml4a_downloads, folder)
-    output_filename = os.path.join(output_folder, filename)
-    if not os.path.exists(output_filename) or overwrite:
+def download_data_file(url, output_path, zip_file=False, overwrite=False):
+    output_folder, output_filename, output_exists = __process_output_path__(output_path, zip_file)
+    if not output_exists or overwrite:
         Path(output_folder).mkdir(parents=True, exist_ok=True)
         with requests.get(url, stream=True) as r:
             with open(output_filename, 'wb') as f:
                 shutil.copyfileobj(r.raw, f)
-    return output_filename
+        if zip_file:
+            unzip(output_filename, output_folder)
+    output = output_folder if zip_file else output_filename
+    return output
 
 
 def download_neural_style(url, output_path):
-    ml4a_downloads = get_ml4a_downloads_folder()
-    folder, filename = os.path.split(output_path)
-    output_folder = os.path.join(ml4a_downloads, folder)
-    output_filename = os.path.join(output_folder, filename)
-    if not os.path.exists(output_filename):
+    output_folder, output_filename, output_exists = __process_output_path__(output_path, False)
+    if not output_exists:
         Path(output_folder).mkdir(parents=True, exist_ok=True)
         sd = model_zoo.load_url(url)
         map_ = {'classifier.1.weight':u'classifier.0.weight', 
@@ -58,3 +64,15 @@ def download_neural_style(url, output_path):
         sd = OrderedDict([(map_[k] if k in map_ else k,v) for k,v in sd.items()])
         torch.save(sd, output_filename)
     return output_filename
+
+
+def __process_output_path__(output_path, zip_file):
+    ml4a_downloads = get_ml4a_downloads_folder()
+    if zip_file:
+        folder, filename = output_path, 'temp.zip'
+    else:
+        folder, filename = os.path.split(output_path)
+    output_folder = os.path.join(ml4a_downloads, folder)
+    output_filename = os.path.join(output_folder, filename)
+    output_exists = os.path.exists(output_folder if zip_file else output_filename)
+    return output_folder, output_filename, output_exists
