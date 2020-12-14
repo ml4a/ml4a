1d1ef3e19abd2333dfc837ce3a44907848278ce5
mayukhdeb <mayukhmainak2000@gmail.com>
2020-12-14 17:53:50 +0530

    setup: add torch_dreams

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ submodules = {
     'stylegan2': ['dnnlib', 'dnnlib.tflib', 'dnnlib.tflib.ops', 'dnnlib.submission', 'dnnlib.submission.internal', 'metrics', 'training'],
     'tacotron2': ['text', 'waveglow'],
     'Wav2Lip': ['evaluation', 'evaluation.scores_LSE', 'face_detection', 'face_detection.detection', 'face_detection.detection.sfd', 'models'],
-    'White-box-Cartoonization': ['index_files', 'test_code', 'test_code.saved_models', 'train_code', 'train_code.selective_search']
+    'White-box-Cartoonization': ['index_files', 'test_code', 'test_code.saved_models', 'train_code', 'train_code.selective_search'],
+    'torch-dreams': ['torch_dreams']
 } 
 install_requires = [
     'bs4', 
@@ -40,7 +41,8 @@ install_requires = [
     'torchvision', 
     'tqdm',
     'unidecode',
-    'yacs'
+    'yacs',
+    "tqdm"
 ]
 
 for submodule, subfolders in submodules.items():
