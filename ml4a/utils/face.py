488601fb2c662b727a5be45137ff0818bec60f95
Gene Kogan <kogan.gene@gmail.com>
2021-03-09 17:12:59 -0500

    face processing notebook

--- ml4a/utils/face.py
+++ ml4a/utils/face.py
@@ -4,11 +4,35 @@ import numpy as np
 import imutils
 import dlib
 import cv2
+import torch
+import torchvision.transforms as transforms
 
 from ..utils import downloads
+from ..models import submodules
+
+#with submodules.localimport('submodules/face-parsing.PyTorch') as _importer:
+with submodules.import_from('face-parsing.PyTorch'):  # localimport fails here    
+    from model import BiSeNet
 
 detector = None
 predictor = None
+parser = None
+
+parsing_labels = {
+    'background': 0, 'skin': 1, 'l_brow': 2, 'r_brow': 3,
+    'l_eye': 4, 'r_eye': 5, 'eye_g': 6, 'l_ear': 7, 'r_ear': 8, 
+    'ear_r': 9, 'nose': 10, 'mouth': 11, 'u_lip': 12, 'l_lip': 13,
+    'neck': 14, 'neck_l': 15, 'cloth': 16, 'hair': 17, 'hat': 18}
+
+parsing_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
+                  [255, 0, 85], [255, 0, 170],
+                  [0, 255, 0], [85, 255, 0], [170, 255, 0],
+                  [0, 255, 85], [0, 255, 170],
+                  [0, 0, 255], [85, 0, 255], [170, 0, 255],
+                  [0, 85, 255], [0, 170, 255],
+                  [255, 255, 0], [255, 255, 85], [255, 255, 170],
+                  [255, 0, 255], [255, 85, 255], [255, 170, 255],
+                  [0, 255, 255], [85, 255, 255], [170, 255, 255]]
 
 
 def setup_face_detection():
@@ -20,7 +44,25 @@ def setup_face_detection():
     predictor = dlib.shape_predictor(predictor_file)
 
     
-def align_face(img, face_width=256, resize_width=800):
+def setup_face_parsing():
+    global parser
+    parser_file = downloads.download_from_gdrive(
+        gdrive_fileid='154JgKpzCPW82qINcVieuPH3fZ2e0P812', 
+        output_path='face-parsing/79999_iter.pth')
+    n_classes = 19
+    parser = BiSeNet(n_classes=n_classes)
+    parser.cuda()
+    parser.load_state_dict(torch.load(parser_file))
+    parser.eval()
+
+
+def get_parsing_labels():
+    return list(parsing_labels.keys())
+
+
+def align_face(img, 
+               face_width=256, 
+               resize_width=800):    
     if not predictor:
         setup_face_detection()
 
@@ -46,7 +88,65 @@ def align_face(img, face_width=256, resize_width=800):
         return None, False
 
     
+def get_face_parts(parsing, labels):
+    labels = labels if isinstance(labels, list) else [labels]
+    labels_found = [label in get_parsing_labels() for label in labels]
+    assert False not in labels_found, 'Error: labels not understood'
+    part_masks = [np.equal(parsing, parsing_labels[label]) for label in labels]
+    full_mask = np.sum(part_masks, axis=0)
+    full_mask = 255 * np.tile(full_mask[:, :, None], [1, 1, 3])
+    return full_mask
+
+
+def parse_face(img):   
+    if not parser:
+        setup_face_parsing()
+        
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(img.astype(np.uint8))
+
+    to_tensor = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    with torch.no_grad():
+        img = to_tensor(img)
+        img = torch.unsqueeze(img, 0)
+        img = img.cuda()
+        out = parser(img)[0]
+        parsing = out.squeeze(0).cpu().numpy().argmax(0)
+        return parsing
+    
+    
+def visualize_parse(parsing, overlay=None):
+    w1, w2, stride = 0.4, 0.6, 1
+    if overlay is not None:
+        vis_im = np.array(overlay).copy().astype(np.uint8)
+    else:
+        vis_im = np.zeros(parsing.shape).astype(np.uint8)
+        w1, w2 = 0, 1
+    
+    vis_parsing_anno = parsing.copy().astype(np.uint8)
+    vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
+    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    num_of_class = np.max(vis_parsing_anno)
+
+    for pi in range(1, num_of_class + 1):
+        index = np.where(vis_parsing_anno == pi)
+        vis_parsing_anno_color[index[0], index[1], :] = parsing_colors[pi]
+
+    vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
+    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), w1, vis_parsing_anno_color, w2, 0)
+    return vis_im
+    
+    
 def align_face_from_path(img_path, face_width=256, resize_width=800):
     img = Image.open(img_path).convert('RGB') 
     x, face_found = align_face(img, face_width, resize_width)
     return x, face_found
+
+
+def parse_face_from_path(img_path):
+    img = Image.open(img_path).convert('RGB') 
+    parse = parse_face(img)
+    return parse
